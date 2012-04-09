7d1bbde77a7e13e557d38b77788015dd89e5b77d
C53 <si.pornchai@gmail.com>
2012-04-09 16:47:59 +0700

    Rename model mcontact.php to mcontacts.php

---
+++ application/models/mcontacts.php
@@ -0,0 +1,18 @@
+<?php
+class MContacts extends CI_Model
+{
+	function __construct()
+	{
+		parent::__construct();
+	}
+	function addContact()
+	{
+		$now=date("Y-m-d H:i:s");
+		$data=array(
+		'name'=>$this->security->xss_clean($this->input->post('name')),
+		'email'=>$this->security->xss_clean($this->input->post('email')),
+		'notes'=>$this->security->xss_clean($this->input->post('notes'))
+		);
+		$this->db->insert('contacts',$data);
+	}
+}
